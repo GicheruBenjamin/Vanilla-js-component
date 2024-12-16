39634267104d9a8d935aae6463049f12e7d049d9
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2024-12-16 12:02:20 +0300

    feature: Init and 1st test

---
+++ app.js
@@ -0,0 +1,8 @@
+const main  = document.querySelector('main');
+const header = document.querySelector('header');
+
+const headerText = document.createTextNode('Js component');
+header.appendChild(headerText);
+
+const mainText = document.createTextNode('Hello world');
+main.appendChild(mainText);
