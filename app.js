e1eb8008e47ed60c64bd1ed2d8ba5798fba5606b
GicheruBenjamin <benjaminnjuguna77@gmail.com>
2024-12-17 04:12:41 +0300

    feature: Sryles file added

--- app.js
+++
@@ -1,8 +0,0 @@
-const main  = document.querySelector('main');
-const header = document.querySelector('header');
-
-const headerText = document.createTextNode('Js component');
-header.appendChild(headerText);
-
-const mainText = document.createTextNode('Hello world');
-main.appendChild(mainText);
